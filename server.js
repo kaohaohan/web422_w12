399f4886a3f9705bcc52666eda6a92e720a9a775
kaohaohan <haohan920@gmail.com>
2025-08-06 18:21:08 -0400

    Replace server.js with a working version

--- server.js
+++ server.js
@@ -4,44 +4,37 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 const userService = require("./user-service.js");
-const passport = require("passport");
-const passportJWT = require("passport-jwt");
-const jwt = require("jsonwebtoken");
-
-// JSON Web Token Strategy
-let ExtractJwt = passportJWT.ExtractJwt;
-let JwtStrategy = passportJWT.Strategy;
 
 const HTTP_PORT = process.env.PORT || 8080;
 
-app.use(express.json());
-app.use(cors());
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
+const passportJWT = require("passport-jwt");
 
-// Configure the JWT Strategy
-let strategyOptions = {
-  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+// JSON Web Token Setup
+let ExtractJwt = passportJWT.ExtractJwt;
+let JwtStrategy = passportJWT.Strategy;
+
+// Configure its options
+let jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme("jwt"),
   secretOrKey: process.env.JWT_SECRET,
 };
 
-let strategy = new JwtStrategy(strategyOptions, (jwt_payload, done) => {
-  userService
-    .getUserById(jwt_payload._id)
-    .then((user) => {
-      if (user) {
-        return done(null, user);
-      } else {
-        return done(null, false);
-      }
-    })
-    .catch((err) => {
-      return done(err, null);
+let strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
+  if (jwt_payload) {
+    next(null, {
+      _id: jwt_payload._id,
+      userName: jwt_payload.userName,
     });
+  } else {
+    next(null, false);
+  }
 });
-
-// Tell passport to use our strategy
 passport.use(strategy);
 
-// Initialize passport
+app.use(express.json());
+app.use(cors());
 app.use(passport.initialize());
 
 app.post("/api/user/register", (req, res) => {
@@ -59,11 +52,12 @@ app.post("/api/user/login", (req, res) => {
   userService
     .checkUser(req.body)
     .then((user) => {
-      const payload = {
+      let payload = {
         _id: user._id,
         userName: user.userName,
       };
-      const token = jwt.sign(payload, process.env.JWT_SECRET);
+
+      let token = jwt.sign(payload, jwtOptions.secretOrKey);
       res.json({ message: "login successful", token: token });
     })
     .catch((msg) => {
